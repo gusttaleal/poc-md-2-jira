22cdb4c43cf69c4aa179217b22df5b531414e50a
gusttaleal <lses.gustavo@gmail.com>
2023-09-08 16:54:09 -0300

    feat: Developing catch tool for extract all tasks from .md file

---
+++ src/getters/getAllTasksFromFileByIndexTasks.js
@@ -0,0 +1,7 @@
+exports.getAllTasksFromFileByIndexTasks = function (file, indexTasks) {
+    let tasks = []
+    for (i = 1; i < indexTasks.length; i++) {
+        tasks.push(file.slice(indexTasks[i - 1], indexTasks[i] - 2))
+    }
+    return tasks;
+}
