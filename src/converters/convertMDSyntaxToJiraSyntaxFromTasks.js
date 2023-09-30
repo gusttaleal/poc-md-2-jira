281944829a47f71ed3dc1e70aec5f967fa9c0436
Gustavo Leal <lses.gustavo@gmail.com>
2023-09-29 21:03:49 -0300

    Update convertMDSyntaxToJiraSyntaxFromTasks.js

--- src/converters/convertMDSyntaxToJiraSyntaxFromTasks.js
+++ src/converters/convertMDSyntaxToJiraSyntaxFromTasks.js
@@ -2,12 +2,14 @@ const { convertBoldSyntax } = require('./convertBoldSyntax.js');
 const { convertCodeSyntax } = require('./convertCodeSyntax.js');
 const { convertLinkSyntax } = require('./convertLinkSyntax.js');
 const { convertSubListSyntax } = require('./convertSubListSyntax.js');
+const { convertSecondSubListSyntax } = require('./convertSecondSubListSyntax.js');
 
 exports.convertMDSyntaxToJiraSyntaxFromTasks = function (tasks) {
     let convertedTasks = []
     for (let task of tasks) {
         task = convertBoldSyntax(task);
         task = convertSubListSyntax(task);
+        task = convertSecondSubListSyntax(task);        
         task = convertLinkSyntax(task);
         task = convertCodeSyntax(task);
         convertedTasks.push(task);
